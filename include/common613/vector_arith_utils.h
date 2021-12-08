f6ee6da7fb0688e50fe411fa35680add391bc795
613_forever <forever_613@yeah.net>
2021-12-08 22:49:49 +0800

    Fixed: Erroneous inclusion guard.

--- include/common613/vector_arith_utils.h
+++ include/common613/vector_arith_utils.h
@@ -2,8 +2,8 @@
 // Copyright (c) 2021 613_forever
 
 #pragma once
-#ifndef DIALOGVIDEOGENERATOR_POS_ARITH_H
-#define DIALOGVIDEOGENERATOR_POS_ARITH_H
+#ifndef COMMON613_VECTOR_ARITH_UTILS_H
+#define COMMON613_VECTOR_ARITH_UTILS_H
 
 #include <functional>
 #include <vector>
@@ -117,4 +117,4 @@ inline bool operator>=(const ArrNi<A, IntT, 1>& lhs, const ArrNi<A, IntT, 1>& rh
 
 }
 
-#endif //DIALOGVIDEOGENERATOR_POS_ARITH_H
+#endif //COMMON613_VECTOR_ARITH_UTILS_H
