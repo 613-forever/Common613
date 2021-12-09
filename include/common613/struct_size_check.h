3af231a50989df1b2d4c1a8d4ae48e663ac5a950
613_forever <forever_613@yeah.net>
2021-12-09 13:45:11 +0800

    Added: check layout for binary interactive types.

--- include/common613/struct_size_check.h
+++ include/common613/struct_size_check.h
@@ -9,6 +9,10 @@
 #define COMMON613_INJECT_SIZE_FIELD(sz) public: constexpr static const std::size_t size = (sz)
 /// @brief Checks if a type has the asserted size of its static const field "size".
 #define COMMON613_CHECK_SIZE(type) static_assert(sizeof(type) == (type::size), "Size is incorrect: " #type ".")
+/// @brief Checks if a type is a POD type.
+#define COMMON613_CHECK_STANDARD_LAYOUT(type) static_assert(std::is_standard_layout<type>::value, "Not a pod: " #type ".")
+/// @brief Checks if a type can be used in binary interactions.
+#define COMMON613_CHECK_BINARY_USABLE(type) COMMON613_CHECK_SIZE(type); COMMON613_CHECK_STANDARD_LAYOUT(type)
 
 namespace common613 {}
 
