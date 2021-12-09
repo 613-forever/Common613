acefd9e35a29d5e39cb0979105016ec5f7a1ab57
613_forever <forever_613@yeah.net>
2021-12-09 22:46:00 +0800

    Fixed: change POD in description to standard layout.

--- include/common613/struct_size_check.h
+++ include/common613/struct_size_check.h
@@ -5,14 +5,23 @@
 #ifndef COMMON613_STRUCT_SIZE_CHECK_H
 #define COMMON613_STRUCT_SIZE_CHECK_H
 
+/// @brief Provides convenient modification for injected field name.
+#define COMMON613_INJECTED_SIZE injected_size
 /// @brief Generates a static const field "size" to mark the size of a struct.
-#define COMMON613_INJECT_SIZE_FIELD(sz) public: constexpr static const std::size_t size = (sz)
+#define COMMON613_INJECT_SIZE_FIELD(sz) \
+public: constexpr static const std::size_t COMMON613_INJECTED_SIZE = (sz)
+/// @brief Generates a static const field "size" to mark the size of a struct.
+#define COMMON613_INHERIT_SIZE_FIELD(parent) \
+public: using parent::COMMON613_INJECTED_SIZE
 /// @brief Checks if a type has the asserted size of its static const field "size".
-#define COMMON613_CHECK_SIZE(type) static_assert(sizeof(type) == (type::size), "Size is incorrect: " #type ".")
-/// @brief Checks if a type is a POD type.
-#define COMMON613_CHECK_STANDARD_LAYOUT(type) static_assert(std::is_standard_layout<type>::value, "Not a pod: " #type ".")
+#define COMMON613_CHECK_SIZE(type) \
+static_assert(sizeof(type) == (type::COMMON613_INJECTED_SIZE), "Size is incorrect: " #type ".")
+/// @brief Checks if a type is a standard-layout type.
+#define COMMON613_CHECK_STANDARD_LAYOUT(type) \
+static_assert(std::is_standard_layout<type>::value, "Not standard layout: " #type ".")
 /// @brief Checks if a type can be used in binary interactions.
-#define COMMON613_CHECK_BINARY_USABLE(type) COMMON613_CHECK_SIZE(type); COMMON613_CHECK_STANDARD_LAYOUT(type)
+#define COMMON613_CHECK_BINARY_USABLE(type) \
+COMMON613_CHECK_SIZE(type); COMMON613_CHECK_STANDARD_LAYOUT(type)
 
 namespace common613 {}
 
